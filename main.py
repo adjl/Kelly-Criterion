6b3cd5877a3d75052a5d728dd4d65976671031d8
Adei Josol <helena.josol@gmail.com>
2019-05-29 06:39:12 +0100

    Prettify messages

--- main.py
+++ main.py
@@ -25,12 +25,20 @@ win_chance: int = int(input('Chance of winning: '))
 min_bankroll: int = calc_bet_size(bankroll, math.pow(0.6, turns))
 max_bankroll: int = calc_max_bankroll(bankroll, win_chance, turns)
 
-print(f'Min bankroll forecast: {min_bankroll:,} (loss of -{bankroll - min_bankroll:,})')
-print(f'Max bankroll forecast: {max_bankroll:,} (profit of +{max_bankroll - bankroll:,})')
+max_loss: int = bankroll - min_bankroll
+max_profit: int = max_bankroll - bankroll
+
+max_loss_percent: float = max_loss / bankroll * 100.0
+max_profit_percent: float = max_profit / bankroll * 100.0
+
+print(' '.join((f'Min bankroll forecast: {min_bankroll:,}',
+                f'(max loss of -{max_loss:,}, -{max_loss_percent:.2f}%)')))
+print(' '.join((f'Max bankroll forecast: {max_bankroll:,}',
+                f'(max profit of +{max_profit:,}, +{max_profit_percent:.2f}%)')))
 
 for i in range(turns):
     optimal_bet: int = calc_bet_size(bankroll, kelly_criterion(win_chance))
-    print(f'{"-" * 70}\nOptimal bet: {optimal_bet}')
+    print(f'{"-" * 70}\nOptimal bet: ({optimal_bet:,}) {optimal_bet}')
 
     if i < turns - 1:
         result: str = input('[W]in or [L]oss: ').upper()
