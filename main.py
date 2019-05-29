88a695a9f8dc18acce4700d8b5e5c4b38e18d742
Adei Josol <helena.josol@gmail.com>
2019-05-29 05:47:50 +0100

    Loss and profit forecasts

--- main.py
+++ main.py
@@ -3,13 +3,35 @@
 import math
 
 
+def kelly_criterion(win_chance: int) -> float:
+    return win_chance / 50.0 - 1.0
+
+
+def calc_bet_size(bankroll: int, fraction: float) -> int:
+    return math.ceil(bankroll * fraction)
+
+
+def calc_max_bankroll(bankroll: int, win_chance: int, turns: int) -> int:
+    for i in range(turns):
+        bankroll += calc_bet_size(bankroll, kelly_criterion(win_chance + i))
+    return bankroll
+
+
+turns: int = 4
+
 bankroll: int = int(input('Bankroll: '))
 win_chance: int = int(input('Chance of winning: '))
-turns: int = 4
+
+min_bankroll: int = calc_bet_size(bankroll, math.pow(0.6, turns))
+max_bankroll: int = calc_max_bankroll(bankroll, win_chance, turns)
+
+print(f'Min bankroll forecast: {min_bankroll:,} (loss of -{bankroll - min_bankroll:,})')
+print(f'Max bankroll forecast: {max_bankroll:,} (profit of +{max_bankroll - bankroll:,})')
+
 for i in range(turns):
-    print('-' * 33)
-    optimal_bet: int = math.ceil(bankroll * (win_chance / 50 - 1))
-    print('Optimal bet: {}'.format(optimal_bet))
+    optimal_bet: int = calc_bet_size(bankroll, kelly_criterion(win_chance))
+    print(f'{"-" * 70}\nOptimal bet: {optimal_bet}')
+
     if i < turns - 1:
         result: str = input('[W]in or [L]oss: ').upper()
         bankroll += optimal_bet if result == 'W' else -optimal_bet
